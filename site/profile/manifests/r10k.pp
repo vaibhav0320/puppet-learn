328d886ae7b82f952a1aae4a6995de7143cbc868
Vaibhav Chauhan <vaibhav.chauhan@newfold.com>
2022-12-07 17:33:27 +0530

    removed webhook from 	modified:   site/profile/manifests/r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -3,13 +3,4 @@ class profile::r10k{
     remote => 'https://github.com/vaibhav0320/puppet-learn',
    }
    
-   class { 'r10k::webhook::config':
-    use_mcollective => false,
-    enable_ssl => false,
-    }
-    
-    class {'r10k::webhook':
-      user => root,
-      group => root,
-      }
-  }
+ }
