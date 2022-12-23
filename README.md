124988c135e11bb614d149f1b16ae600e81bffab
Vaibhav Chauhan <vaibhav.chauhan@newfold.com>
2022-12-23 17:34:12 +0530

    Removed everything

--- README.md
+++
@@ -1,1 +0,0 @@
-Puppet Learning excersice
--- manifests/site.pp
+++
@@ -1,7 +0,0 @@
-node puppet.local {
-  include role::master
-  file {'/etc/secret_password.txt':
-	ensure => file,
-	content => lookup('secret'),
-   }
- }
